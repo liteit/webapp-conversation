f0acb1151699de9def7ff5b48349393fefb0f796
liteit <cloud@liteit.se>
2024-10-12 00:27:32 +0200

    fix

--- config/index.ts
+++ config/index.ts
@@ -3,14 +3,17 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'SkolAI',
-  description: 'Experimental',
+
+  title: 'SkolAI - SaaS Startup Mentor',
+  description: 'Now you can generate and refine a truly innovative, high-potential SaaS product idea. We are eager to help you as an aspiring entrepreneur. This is our contribution to make dreams come to life! Full speed ahead Ali, Nicklas, Onat and Magnus',
   copyright: 'SkolAI',
-  privacy_policy: '',
+  privacy_policy: 'To Be Defined',
   default_language: 'en',
+
 }
 
 export const isShowPrompt = false
+
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
